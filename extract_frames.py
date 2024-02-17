ef0532a0cd284fc28d2319c7a4d8b9cef9cb1b9e
RONDEL-Ewen <ewen.rondel@edu.devinci.fr>
2024-02-17 10:24:23 +0100

    First try to extract frames from videos

---
+++ extract_frames.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import numpy as np
+import subprocess
+import pickle
+import h5py
+import cv2
+import sys
+import os
+
+def get_args():
+    """
+    Parse 'name=value' command line arguments.
+    """
+
+    args = {}
+
+    for arg in sys.argv[1:]:
+
+        key, value = arg.split('=')
+        args[key] = value
+
+    return args
+
+def video_to_frames(
+    video_path: str,
+    frames_folder: str
+):
+    """
+    Cut a video in frames.
+    """
+
+    if not os.path.exists(frames_folder):
+        os.makedirs(frames_folder)
+
+    command = f"ffmpeg -i {video_path} -vf \"scale=456:256\" -q:v {frames_folder}/frame_%010d.jpg"
+    subprocess.run(command, shell = True)
+
+def main():
+
+    args = get_args()
+    video_path = args.get('video_path', None)
+    frames_folder = args.get('frames_folder', None)
+
+    if video_path and frames_folder:
+        video_to_frames(video_path, frames_folder)
+    else:
+        print("Missing one argument")
+
+if __name__ == '__main__':
+    main()
