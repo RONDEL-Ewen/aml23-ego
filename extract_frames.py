d4eb9bf0dbeafbdf73cd4f13e7e2efd54dfae765
RONDEL-Ewen <ewen.rondel@edu.devinci.fr>
2024-02-17 10:38:11 +0100

    Fix for the frames extraction

--- extract_frames.py
+++ extract_frames.py
@@ -32,7 +32,7 @@ def video_to_frames(
     if not os.path.exists(frames_folder):
         os.makedirs(frames_folder)
 
-    command = f"ffmpeg -i {video_path} -vf \"scale=456:256\" -q:v {frames_folder}/frame_%010d.jpg"
+    command = f"ffmpeg -i {video_path} -vf \"scale=456:256\" -q:v 2 {frames_folder}/frame_%010d.jpg"
     subprocess.run(command, shell = True)
 
 def main():
